f922e72102b3b079a5b0c915ccbed0b0462c8859
matkolis <anete.priekule@gmail.com>
2023-02-09 18:08:17 +0200

    fix #11

--- 0 saknes/polysaknes.m
+++ 0 saknes/polysaknes.m
@@ -1,10 +1,12 @@
 clc, clearvars,format compact
 syms x
-f = cos(x-0.2) - x./5-0.9;
+f = x.^4-x.^3-3.*x.^2+0.8;
 fplot(f, [-20 20])
 figure
-fplot(f, [-10 5])
-% 5 saknes redzamas.
+fplot(f, [-5 5])
+figure
+fplot(f, [-2 3])
+% 4 saknes redzamas.
 grid on
 vpasolve(f, x)
 roots(f)
