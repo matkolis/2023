c97ebaf8ef9c540bded7735c2ad1e24c96cd5a3e
matkolis <anete.priekule@gmail.com>
2023-02-09 22:04:59 +0200

    varbut #53

--- A matricas/5.2 Jakobi/053_Jakobi.m
+++ A matricas/5.2 Jakobi/053_Jakobi.m
@@ -1,12 +1,12 @@
 %% 1.uzd Jakobi
 clc, clearvars, format compact, format short 
-A=[7 3 2 6
-2 5 -1 2
-3 1 8 2
-4 -2 1 9]; B=[3
--1
-2
-5];
+A=[3 1 -1 2
+2 5 3 1
+3 -2 6 1
+4 1 1 8]; B=[-1
+-2
+4
+0];
 if det(A) == 0
     disp('Matrica A ir singulârâ ')
     disp(' Atbilde:  Jakobi metodi nedrîkst izmantot'), return
@@ -16,7 +16,7 @@ fun_prob3(A) % pârbaude: vai Jakobi metode konverìç?
 
 % turpinâjums 
 n = length(B);
-x_app = zeros(n ,1); itermax = 7; k = 1; 
+x_app = zeros(n ,1); itermax = 8; k = 1; 
 errnorm =1; prnorm =zeros(1,2);
 for iter = 1:itermax
  k = k+1;
@@ -37,7 +37,7 @@ for i = 1:n
    x_7(i)= x_app(i, 8);
 end
 %norm(B-A*x_app(:8))
-norm(B-A*x_7)
+%norm(B-A*x_7)
 %disp('x4 9. iterācijā')
 %x_app(4,10)
 % x_app
@@ -47,9 +47,9 @@ disp(prnorm)
 x_approx = x_app(:,k);
 %}
 
-%X = linsolve(A,B);
+X = linsolve(A,B);
 %X(4)
-%sol_X12_norm = norm(X-x_app(:,13))
+sol_X12_norm = norm(X-x_app(:,9))
 
 %X = linsolve(A,B)
 %sol_X12_norm = norm(X - x_app(:, itermax+1)) % 4 skaitli aiz komata
