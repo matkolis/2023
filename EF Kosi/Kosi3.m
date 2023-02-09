b8a4934032e674f04ef2fc2e6cd47e89940eb90f
matkolis <anete.priekule@gmail.com>
2023-02-09 15:18:39 +0200

    fix #6

--- EF Kosi/Kosi3.m
+++ EF Kosi/Kosi3.m
@@ -2,17 +2,15 @@
 clc, clearvars, format compact 
 %--------------------------------------------
 dy_dx = @(x,y) [
-    y(2);
-    y(3);
-    -(3+acot(x)).*y(3)-sqrt(1+x+x.^3).*y(2)-log(2+x.^2).*y(1)+sin(x)
+    (-(nthroot(x, 3)./(x+1)+sin(3.*x)).*y(1) + (x.^2+1)./(x.^3+4))./(exp(sqrt(x))+x.^2+2)
     ];                  
 %--------------------------------------------
 
 % Define value at y(lower_lim)
-y0 = [4; 5; 8];                     
+y0 = [2];                     
 
 % Define interval for x
-lower_lim = 1;
+lower_lim = 0;
 upper_lim = 7;
 %delta = 0.1;    % might be usefull later
 
@@ -22,14 +20,14 @@ x_int = [lower_lim upper_lim];
 sol = ode45(dy_dx,x_int,y0);
 
 %Define point of interest
-pnt = 3.72;
+pnt = 2.38;
 
 % Value of y at point of interest 
 y_pnt = deval(sol,pnt);
 
 fprintf('Answer: y(%.2f) = %.4f \n ',pnt ,y_pnt(1))
-fprintf("Answer: y'(%.2f) = %.4f \n ",pnt ,y_pnt(2))
-fprintf("Answer: y''(%.2f) = %.4f \n ",pnt ,y_pnt(3))
+%fprintf("Answer: y'(%.2f) = %.4f \n ",pnt ,y_pnt(2))
+%fprintf("Answer: y''(%.2f) = %.4f \n ",pnt ,y_pnt(3))
 
 %{
  Graph if needed
