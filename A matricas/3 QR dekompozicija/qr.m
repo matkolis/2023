78661da736918416a340823d7cca7bfa01617cf7
matkolis <anete.priekule@gmail.com>
2023-02-09 18:15:36 +0200

    fix #12

--- A matricas/3 QR dekompozicija/qr.m
+++ A matricas/3 QR dekompozicija/qr.m
@@ -5,15 +5,19 @@ kā reizinājumu
 %}
 %% 15.piemērs.QR dekompozīcija ( atstarošanās metode )
 clc, clearvars, format compact
-A = [1,2,6;2,7,3;6,3,64]; B = [7;2;13];
+A = [4 5 1 6
+    3 -1 2 8
+    2 2 5 7
+    -2 4 -1 6]; %B = [7;2;13];
 if det(A) == 0
  disp('Matrica A ir singulārā ')
  disp(' Atbilde: astarošanas metodi nedrīkst izmantot'), return
 end
 disp disp('Matrica A ir nesingulārā')
 [Q,R] = qr(A)
-Amat =  abs(Q*R)
-X = inv(R)*inv(Q)*B
+%Amat =  abs(Q*R)
+%X = inv(R)*inv(Q)*B
 % turpinājums
 disp('Atbilde:')
-fprintf(' x1 = %.0f; x2 = %.0f; x3 = %.0f\n',X)
+disp(R(2,2))
+%fprintf(' x1 = %.0f; x2 = %.0f; x3 = %.0f\n',X)
